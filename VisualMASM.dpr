1d0a2aceccc95a478d3433bfbab6e68057144c01
Thomas Jaeger <thomasjaeger@gmail.com>
2017-01-16 11:29:36 -0500

    Added base components, icon, etc.

--- VisualMASM.dpr
+++ VisualMASM.dpr
@@ -2,13 +2,16 @@ program VisualMASM;
 
 uses
   Vcl.Forms,
-  uFrmMain in 'uFrmMain.pas' {frmMain};
+  uFrmMain in 'uFrmMain.pas' {frmMain},
+  uDM in 'uDM.pas' {dm: TDataModule};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  Application.Title := 'VisualMASM';
   Application.CreateForm(TfrmMain, frmMain);
+  Application.CreateForm(Tdm, dm);
   Application.Run;
 end.
---
+++ uDM.pas
@@ -0,0 +1,109 @@
+unit uDM;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, acPathDialog, sDialogs, Vcl.Dialogs,
+  System.Actions, Vcl.ActnList, Vcl.XPStyleActnCtrls, Vcl.ActnMan, Vcl.ImgList,
+  Vcl.Controls, acAlphaImageList;
+
+type
+  Tdm = class(TDataModule)
+    iml32x32Icons: TsAlphaImageList;
+    iml64x64Icons: TsAlphaImageList;
+    sAlphaImageList1: TsAlphaImageList;
+    ActionManager1: TActionManager;
+    actOptions: TAction;
+    actFileNew32BitWindowsExeAppAddToGroup: TAction;
+    actAbout: TAction;
+    actNew32BitWindowsDllApp: TAction;
+    actNew64BitWindowsExeApp: TAction;
+    actNew64BitWindowsDllApp: TAction;
+    actNew16BitDOSComApp: TAction;
+    actSearchFind: TAction;
+    actFindInFiles: TAction;
+    actSearchReplace: TAction;
+    actSearchAgain: TAction;
+    actSearchPrevious: TAction;
+    actGoToLineNumber: TAction;
+    actWelcomePage: TAction;
+    actHelpInstallingVisualMASM: TAction;
+    actGettingStarted: TAction;
+    actHelloWorldMSDOS: TAction;
+    actHelloWorldWindows: TAction;
+    actAskQuestionsProvideFeedback: TAction;
+    actNew16BitDOSExeApp: TAction;
+    actNew16BitWindowsExeApp: TAction;
+    actNew16BitWindowsDllApp: TAction;
+    actResources: TAction;
+    actClosePage: TAction;
+    actAddNewAssemblyFile: TAction;
+    actViewProjectManager: TAction;
+    actViewOutput: TAction;
+    actViewCommandLine: TAction;
+    actProperties: TAction;
+    actNewOther: TAction;
+    actAddNewTextFile: TAction;
+    actAddToProject: TAction;
+    actAddNewBatchFile: TAction;
+    actNewProjectGroup: TAction;
+    actOpenProject: TAction;
+    actShowInExplorer: TAction;
+    actDOSPromnptHere: TAction;
+    actRemoveFromProject: TAction;
+    actDeleteFile: TAction;
+    actSave: TAction;
+    actAddNewProject: TAction;
+    actAddExistingProject: TAction;
+    actNew16BitDOSComAppAddToGroup: TAction;
+    actEditUndo: TAction;
+    actEditRedo: TAction;
+    actEditCut: TAction;
+    actEditCopy: TAction;
+    actEditPaste: TAction;
+    actCopyPath: TAction;
+    actReopenFile: TAction;
+    actAssembleFile: TAction;
+    actProjectBuild: TAction;
+    actProjectOptions: TAction;
+    actFileRename: TAction;
+    actProjectRename: TAction;
+    actGroupRename: TAction;
+    actProjectAssemble: TAction;
+    actProjectSave: TAction;
+    actProjectSaveAs: TAction;
+    actGroupSaveAs: TAction;
+    actFileSaveAs: TAction;
+    actGroupRemoveProject: TAction;
+    actGroupSave: TAction;
+    actProjectRun: TAction;
+    actFileSaveAll: TAction;
+    actEditDelete: TAction;
+    actEditCommentLine: TAction;
+    actEditSelectAll: TAction;
+    actFileOpen: TAction;
+    actProjectMakeActiveProject: TAction;
+    actFileClose: TAction;
+    actFileCloseAll: TAction;
+    actGroupAssembleAllProjects: TAction;
+    actGroupBuildAllProjects: TAction;
+    ImageList1: TImageList;
+    dlgSave: TsSaveDialog;
+    dlgOpen: TsOpenDialog;
+    dlgPath: TsPathDialog;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  dm: Tdm;
+
+implementation
+
+{%CLASSGROUP 'Vcl.Controls.TControl'}
+
+{$R *.dfm}
+
+end.
